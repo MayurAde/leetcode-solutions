8597750c0f1ee1ffac414df97499b4c08184cd53
Mayur Ade <53948708+MayurAde@users.noreply.github.com>
2023-07-18 12:41:16 +0530

    Update pascals-triangle.py
    Pascal's triangle source code

--- easy/pascals-triangle.py
+++ easy/pascals-triangle.py
@@ -1,1 +1,20 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        
+        arr = []
 
+        for row in range(0,numRows):
+            
+            temp = []
+
+            for col in range(row+1):
+
+                if col ==0 or col == row:
+                    temp.append(1)
+                else:
+                    val = arr[-1][col-1] + arr[-1][col]
+                    temp.append(val)
+
+            arr.append(temp)   
+            
+        return arr
